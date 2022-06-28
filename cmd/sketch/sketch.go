804e7967fd51972650b0d87af0b20663f48e1fb9
Kallen <keleix@gmail.com>
2022-06-29 06:41:24 +0800

    feat: swagger middleware for gin

--- cmd/sketch/sketch.go
+++ cmd/sketch/sketch.go
@@ -20,6 +20,14 @@ var (
 	ver = flag.Bool("version", false, "show the binary build version")
 )
 
+//go:generate swag init -g sketch.go -o ../../pkg/swagger
+
+// @title Sketch API
+// @version 1.0
+// @BasePath /v1
+// @license.name Apache 2.0
+// @description Sketch API Server
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 	flag.Parse()
 	showVersion()
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -6,9 +6,14 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	swagFile "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 
 	"sketch/pkg/middleware"
 	"sketch/pkg/version"
+
+	// Swagger Annotation Docs
+	_ "sketch/pkg/swagger"
 )
 
 var app *gin.Engine
@@ -33,12 +38,21 @@ func initApp() {
 
 func initRouter() {
 	app.Any("/", index)
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swagFile.Handler))
 
 	// Endpoints For v1
 	v1 := app.Group("/v1")
 	v1.Any("/", index)
 }
 
+// index godoc
+// @Summary index
+// @Description index
+// @Tags index
+// @ID /index
+// @Accept json
+// @Produce json
+// @Success 200
 func index(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"app":    version.AppName,
